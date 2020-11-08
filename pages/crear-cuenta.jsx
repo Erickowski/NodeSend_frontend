022ae03ded17e6eeaf88eee005c9f01137e7ada9
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-08 17:51:22 -0600

    Layout de crear cuenta

--- pages/crear-cuenta.jsx
+++ pages/crear-cuenta.jsx
@@ -3,7 +3,67 @@ import Layout from "../components/Layout";
 export default function CrearCuenta() {
   return (
     <Layout>
-      <h1>Crear cuenta</h1>
+      <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+        <h2 className="text-4xl font-sans font-bold text-gray-800 text-center my-4">
+          Crear cuenta
+        </h2>
+        <div className="flex justify-center mt-5">
+          <div className="w-full max-w-lg">
+            <form
+              action=""
+              className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+            >
+              <div className="mb-4">
+                <label
+                  htmlFor="nombre"
+                  className="block text-black text-sm font-bold mb-2"
+                >
+                  Nombre
+                </label>
+                <input
+                  type="text"
+                  className="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="nombre"
+                  placeholder="Nombre del usuario"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-black text-sm font-bold mb-2"
+                >
+                  Email
+                </label>
+                <input
+                  type="text"
+                  className="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  placeholder="Email del usuario"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="password"
+                  className="block text-black text-sm font-bold mb-2"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  placeholder="Password del usuario"
+                />
+              </div>
+              <input
+                type="submit"
+                className="bg-red-500 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold"
+                value="Crear cuenta"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
