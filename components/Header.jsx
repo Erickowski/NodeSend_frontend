4dabe35facf309ead5005363a8e0260961fdca45
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-08 17:31:48 -0600

    Routing

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="py-8 flex flex-col md:flex-row items-center justify-between">
-      <img src="logo.svg" alt="Logo" className="w-64 mb-8 md:mb-0" />
+      <Link href="/">
+        <img src="logo.svg" alt="Logo" className="w-64 mb-8 md:mb-0" />
+      </Link>
+      <div>
+        <Link href="/login">
+          <a className="bg-red-500 px-5 py-3 rounded-lg text-white font-bold uppercase mr-2">
+            Iniciar sesión
+          </a>
+        </Link>
+        <Link href="/crear-cuenta">
+          <a className="bg-black px-5 py-3 rounded-lg text-white font-bold uppercase">
+            Crear cuenta
+          </a>
+        </Link>
+      </div>
     </header>
   );
 };
---
+++ pages/crear-cuenta.jsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function CrearCuenta() {
+  return (
+    <Layout>
+      <h1>Crear cuenta</h1>
+    </Layout>
+  );
+}
